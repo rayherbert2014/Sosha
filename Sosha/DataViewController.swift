ba6d4684ac52da255af71e1a07637a1abfa0ffa6
rayherbert2014 <rayherbert2014@gmail.com>
2018-02-19 22:10:19 -0600

    Initial Commit

---
+++ Sosha/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Sosha
+//
+//  Created by Ray Herbert on 2/19/18.
+//  Copyright © 2018 Ray Herbert. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
